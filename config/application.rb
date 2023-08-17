83b8537b9b34daf3b307d6135694cbca69b583aa
Lionel Palacin <lionel.palacin@gmail.com>
2023-08-17 16:12:33 +0200

    add docker environment file

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module Elastic
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -29,5 +31,11 @@ module Elastic
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
